842f82be489ac85ac4a9c20314f0cbe7ddd8c74b
Sanka Weerathunga <sdweerathunga5@gmail.com>
2017-04-06 11:14:50 +0530

    add scritp4.js

--- script4.js
+++ script4.js
@@ -1,18 +1,10 @@
-//var server = require('http');
-//server.createServer(engine).listen(1337); //leets
+var server = require('http');
 
-var http = require('http');
+server.createServer(engine).listen(1337); //leets
 
-var server = http.createServer(engine);
-
-server.listen(1337,function(){
-    console.log('Server was hit by a request');
-});
- 
 function engine(request, response){
 
-    console.log(response);
-    //response.writeHead(200, {"Content-type":'text/plain'});
+    response.writeHead(200, {"Content-type":'text/plain'});
 
-    //response.end('hey there, from the server ! :D');
+    response.end('hey there, from the server ! :D');
 }
