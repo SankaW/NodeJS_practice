45b029c282a99b0a607cb8bbfc12110697076555
Sanka Weerathunga <sdweerathunga5@gmail.com>
2017-04-06 11:03:12 +0530

    Do changes to script4

--- script4.js
+++ script4.js
@@ -1,9 +1,18 @@
-var server = require('http');
+//var server = require('http');
+//server.createServer(engine).listen(1337); //leets
 
-server.createServer(engine).listen(1337); //leets
+var http = require('http');
 
+var server = http.createServer(engine);
+
+server.listen(1337,function(){
+    console.log('Server was hit by a request');
+});
+ 
 function engine(request, response){
-    response.writeHead(200, {"Content-type":'text/plain'});
 
-    response.end('hey there, from the server ! :D');
+    console.log(response);
+    //response.writeHead(200, {"Content-type":'text/plain'});
+
+    //response.end('hey there, from the server ! :D');
 }
