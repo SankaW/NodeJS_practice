08dd7a7dd2ea324fcdc2163f1f04fb16fd7af4fb
Sanka Weerathunga <sdweerathunga5@gmail.com>
2017-04-07 16:39:41 +0530

    Tutorial 14

--- script13.js
+++ script13.js
@@ -1,3 +1,10 @@
 var express = require('express');
-
 var app=express();
+
+app.get('/helloThere', function(req ,res){
+    res.send('Hello there, fromexpress !');
+});
+
+app.listen(1234 , function(){
+    console.log('Listening at Port 1234');
+});
